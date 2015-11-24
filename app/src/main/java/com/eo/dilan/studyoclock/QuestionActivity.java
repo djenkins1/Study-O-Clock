5b18cb109c109bd7e7556fda02c8848509857455
Dilan Jenkins <djenkins1@email.arizona.edu>
2015-11-24 12:57:49 -0700

    Fixed backgrounds on buttons and added animation to answer clicks

--- app/src/main/java/com/eo/dilan/studyoclock/QuestionActivity.java
+++ app/src/main/java/com/eo/dilan/studyoclock/QuestionActivity.java
@@ -1,14 +1,19 @@
 package com.eo.dilan.studyoclock;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -71,7 +76,7 @@ public class QuestionActivity extends AppCompatActivity
 			Answer answer = question.answers.get(i);
 			boolean yes = (answer.isCorrect > 0);
 			button.setText(answer.answer);
-			new ClickHandler(button, this, yes);
+			new ClickHandler(button, yes);
 		}
 
 		for ( int i = min; i < buttons.size(); i++ )
@@ -93,31 +98,83 @@ public class QuestionActivity extends AppCompatActivity
 		vibrator.cancel();   // or cancel here
 	}
 
+	private void animateMyView(final View view, final boolean isCorrect )
+	{
+		final float[] from = new float[3],
+				to =   new float[3];
+
+		String color = ( isCorrect ? "green" :  "red" );
+		Color.colorToHSV(Color.parseColor("white"), from);   // from white
+		Color.colorToHSV(Color.parseColor( color ), to);     // to red
+
+		ValueAnimator anim = ValueAnimator.ofFloat(0, 1);   // animate from 0 to 1
+		anim.setDuration(500);                              // for 300 ms
+
+		final float[] hsv  = new float[3];                  // transition color
+		anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener(){
+			@Override public void onAnimationUpdate(ValueAnimator animation) {
+				// Transition along each axis of HSV (hue, saturation, value)
+				hsv[0] = from[0] + (to[0] - from[0])*animation.getAnimatedFraction();
+				hsv[1] = from[1] + (to[1] - from[1])*animation.getAnimatedFraction();
+				hsv[2] = from[2] + (to[2] - from[2])*animation.getAnimatedFraction();
+
+				view.setBackgroundColor(Color.HSVToColor(hsv));
+			}
+		});
+
+		anim.addListener(new AnimatorListenerAdapter()
+		{
+			@Override
+			public void onAnimationStart(Animator arg0)
+			{
+			}
+
+			@Override
+			public void onAnimationRepeat(Animator arg0)
+			{
+			}
+
+			@Override
+			public void onAnimationEnd(Animator arg0)
+			{
+				view.setBackgroundResource(android.R.drawable.btn_default);
+				handleQuestionEnd( isCorrect );
+			}
+		});
+
+		anim.start();
+	}
+
+	private void handleQuestionEnd( final boolean isCorrect )
+	{
+		final Vibrator vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+		//Toast.makeText(you , myText, Toast.LENGTH_SHORT).show();
+		//long[] correctPattern = {0, 100, 50, 100};
+		//long[] incorrect = { 0, 300, 50, 0 };
+		//vibrator.vibrate( ( isCorrect ? correctPattern : incorrect ), -1); //-1 is important
+		db.incrementQuestion();
+		totalNeeded += ( isCorrect ? -1 : 0 );
+		if ( totalNeeded == 0 && isAlarm )
+		{
+			vib.cancel();
+			Intent intent = new Intent( me, MainActivity.class );
+			me.startActivity( intent );
+			return;
+		}
+		updateQuestion(db.getCurrentQuestion() );
+	}
+
 	private class ClickHandler
 	{
-		public ClickHandler( Button button, QuestionActivity me, boolean correct )
+		public ClickHandler( Button button, boolean correct )
 		{
-			final QuestionActivity you = me;
+			//button.setBackgroundResource(android.R.drawable.btn_default);
 			final boolean isCorrect = correct;
 			button.setOnClickListener(new View.OnClickListener()
 			{
 				public void onClick(View v)
 				{
-					final Vibrator vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-					//Toast.makeText(you , myText, Toast.LENGTH_SHORT).show();
-					//long[] correctPattern = {0, 100, 50, 100};
-					//long[] incorrect = { 0, 300, 50, 0 };
-					//vibrator.vibrate( ( isCorrect ? correctPattern : incorrect ), -1); //-1 is important
-					db.incrementQuestion();
-					totalNeeded += ( isCorrect ? -1 : 0 );
-					if ( totalNeeded == 0 && isAlarm )
-					{
-						vib.cancel();
-						Intent intent = new Intent( you, MainActivity.class );
-						you.startActivity( intent );
-						return;
-					}
-					updateQuestion(db.getCurrentQuestion() );
+					animateMyView(v , isCorrect );
 				}
 			});
 		}
