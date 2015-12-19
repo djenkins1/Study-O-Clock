54f0a4a1a05c77a97745b3211467badf1c01039e
Dilan Jenkins <djenkins1@email.arizona.edu>
2015-12-18 23:11:01 -0700

    Added animation for list viewer on click

--- app/src/main/java/com/eo/dilan/studyoclock/AllQuestionsActivity.java
+++ app/src/main/java/com/eo/dilan/studyoclock/AllQuestionsActivity.java
@@ -1,7 +1,12 @@
 package com.eo.dilan.studyoclock;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,20 +16,17 @@ import android.widget.FrameLayout;
 import android.widget.ListView;
 
 import com.eo.dilan.studyoclock.database.DataHelper;
-import com.eo.dilan.studyoclock.database.Logger;
 import com.eo.dilan.studyoclock.database.Question;
 
 import java.util.ArrayList;
 
-public class AllQuestionsActivity extends AppCompatActivity
-{
+public class AllQuestionsActivity extends AppCompatActivity {
 	private DataHelper db;
 
 	private final AllQuestionsActivity me = this;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 		setContentView(R.layout.activity_all_questions);
@@ -32,52 +34,84 @@ public class AllQuestionsActivity extends AppCompatActivity
 	}
 
 	@Override
-	public void onBackPressed()
-	{
+	public void onBackPressed() {
 		super.onBackPressed();
-		Intent intent = new Intent( this, MainActivity.class);
+		Intent intent = new Intent(this, MainActivity.class);
 		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 		startActivity(intent);
 	}
 
 	@Override
-	public void onDestroy()
-	{
+	public void onDestroy() {
 		super.onDestroy();
-		if ( db != null )
-		{
+		if (db != null) {
 			db.clearQuestionsInMem();
 		}
 
 	}
 
-	private void updateList( final ArrayList<Question> questions )
-	{
+	private void updateList(final ArrayList<Question> questions) {
 		final ListView listview = (ListView) findViewById(R.id.listview);
 
 		final ArrayList<String> list = new ArrayList<>();
-		for (int i = 0; i < questions.size(); ++i)
-		{
-			list.add( questions.get( i ).question );
+		for (int i = 0; i < questions.size(); ++i) {
+			list.add(questions.get(i).question);
 		}
-		final MySimpleArrayAdapter adapter = new MySimpleArrayAdapter( this, list, questions );
-		FrameLayout footerLayout = (FrameLayout ) getLayoutInflater().inflate(R.layout.footer_res,null);
+		final MySimpleArrayAdapter adapter = new MySimpleArrayAdapter(this, list, questions);
+		FrameLayout footerLayout = (FrameLayout) getLayoutInflater().inflate(R.layout.footer_res, null);
 		listview.addFooterView(footerLayout);
 		listview.setAdapter(adapter);
 
-		listview.setOnItemClickListener(new AdapterView.OnItemClickListener()
-		{
+		listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
 			@Override
-			public void onItemClick(AdapterView< ? > parent, final View view, int position, long id)
-			{
-				Logger.print(me.getApplicationContext(),"Question Clicked", questions.get(position).question );
-				long quest = questions.get(position).id;
-				Intent appIntent = new Intent(me, AddQuestionActivity.class);
-				Bundle mBundle = new Bundle();
-				mBundle.putLong("question", quest );
-				appIntent.putExtras(mBundle);
-				startActivity(appIntent);
+			public void onItemClick(AdapterView<?> parent, final View view, final int position, long id) {
+				final float[] from = new float[3],
+						to = new float[3];
+
+				final int color = ((ColorDrawable) view.getBackground()).getColor();
+				Color.colorToHSV(Color.parseColor("#5CCCCC"), to);
+				Color.colorToHSV(color, from);
+
+				ValueAnimator anim = ValueAnimator.ofFloat(0, 1);
+				anim.setDuration(100);
+
+				final float[] hsv = new float[3];
+				anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+					@Override
+					public void onAnimationUpdate(ValueAnimator animation) {
+						// Transition along each axis of HSV (hue, saturation, value)
+						hsv[0] = from[0] + (to[0] - from[0]) * animation.getAnimatedFraction();
+						hsv[1] = from[1] + (to[1] - from[1]) * animation.getAnimatedFraction();
+						hsv[2] = from[2] + (to[2] - from[2]) * animation.getAnimatedFraction();
+
+						view.setBackgroundColor(Color.HSVToColor(hsv));
+					}
+				});
+
+				anim.addListener(new AnimatorListenerAdapter() {
+					@Override
+					public void onAnimationStart(Animator arg0) {
+					}
+
+					@Override
+					public void onAnimationRepeat(Animator arg0) {
+					}
+
+					@Override
+					public void onAnimationEnd(Animator arg0) {
+						view.setBackgroundColor(color);
+						long quest = questions.get(position).id;
+						Intent appIntent = new Intent(me, AddQuestionActivity.class);
+						Bundle mBundle = new Bundle();
+						mBundle.putLong("question", quest);
+						appIntent.putExtras(mBundle);
+						startActivity(appIntent);
+					}
+				});
+
+				anim.start();
+
 			}
 
 		});
